dfdc333d2e5a9cc21ff878d608ddbee075c06710
Thomas Nelson <thomasvnelson@live.com>
2019-08-28 21:29:24 -0400

    Added IRelationalDatabaseEntity and updated nuget package

---
+++ SimpleLoggerInterfaces/SimpleLoggingInterfaces/Interfaces/IRelationalDatabaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleLoggingInterfaces.Interfaces
+{
+    public interface IRelationalDatabaseEntity : IEntityBase
+    {
+        string Query { get; set; }
+        string Database { get; set; }
+        string QueryResult { get; set; }
+    }
+}
